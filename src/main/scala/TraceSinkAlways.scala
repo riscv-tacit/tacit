b7092a22a14330d5d613c87056d47f4126565223
Lux <iansseijelly@berkeley.edu>
2025-04-04 18:27:48 -0700

    ADD: HPM Sink builders

--- src/main/scala/TraceSinkAlways.scala
+++ src/main/scala/TraceSinkAlways.scala
@@ -35,3 +35,21 @@ class WithTraceSinkAlways(targetId: Int = 0) extends Config((site, here, up) => 
     case other => other
   }
 })
+
+class WithHPMSinkAlways(targetId: Int = 0) extends Config((site, here, up) => {
+  case TilesLocated(InSubsystem) => up(TilesLocated(InSubsystem), site) map {
+    case tp: RocketTileAttachParams => {
+      tp.copy(tileParams = tp.tileParams.copy(
+        traceParams = Some(tp.tileParams.traceParams.get.copy(buildSinks = 
+          tp.tileParams.traceParams.get.buildSinks :+ (p => (LazyModule(new TraceSinkAlways()(p)), targetId)))))
+      )
+    }
+    case tp: ShuttleTileAttachParams => {
+      tp.copy(tileParams = tp.tileParams.copy(
+        traceParams = Some(tp.tileParams.traceParams.get.copy(buildSinks = 
+          tp.tileParams.traceParams.get.buildSinks :+ (p => (LazyModule(new TraceSinkAlways()(p)), targetId)))))
+      )
+    }
+    case other => other
+  }
+})
