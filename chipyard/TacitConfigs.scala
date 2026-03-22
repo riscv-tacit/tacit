64ff8ef0fdbf688c84881f02ddd88cb0eac22625
Lux <iansseijelly@berkeley.edu>
2026-03-21 17:09:26 -0700

    fix: make more space to backpressure earlier.

--- chipyard/TacitConfigs.scala
+++ chipyard/TacitConfigs.scala
@@ -52,7 +52,7 @@ class WithTacitEncoder extends Config((site, here, up) => {
           iaddrWidth = tp.tileParams.core.xLen
         ),
         bufferDepth = 64,
-        coreStages = 5, // BOOM stalls commit, not fetch
+        coreStages = 10, // BOOM stalls commit, not fetch
         )(p)),
         useArbiterMonitor = false
       )),
@@ -67,7 +67,7 @@ class WithTacitEncoder extends Config((site, here, up) => {
           iaddrWidth = tp.tileParams.core.xLen
         ),
         bufferDepth = 64,
-        coreStages = 5, // BOOM stalls commit, not fetch
+        coreStages = 10, // BOOM stalls commit, not fetch
         )(p)),
         useArbiterMonitor = false
       )),
@@ -182,9 +182,33 @@ class TacitLargeBoomV3RawByteConfig extends Config(
   new chipyard.config.WithSystemBusWidth(128) ++
   new chipyard.config.AbstractConfig)
 
+class TacitDualLargeBoomV3RawByteConfig extends Config(
+  new tacit.WithTraceSinkRawByte(2) ++
+  new tacit.WithTraceSinkDMA(1) ++
+  new tacit.WithTraceSinkAlways(0) ++
+  new chipyard.config.WithTraceArbiterMonitor ++
+  new chipyard.WithTacitEncoder ++
+  // new freechips.rocketchip.subsystem.WithoutTLMonitors ++
+  new chipyard.config.WithAsidLen(16) ++
+  new boom.v3.common.WithNLargeBooms(2) ++
+  new chipyard.config.WithSystemBusWidth(128) ++
+  new chipyard.config.AbstractConfig)
+
 class TacitMegaBoomV3RawByteConfig extends Config(
   new tacit.WithTraceSinkRawByte(2) ++
   new tacit.WithTraceSinkDMA(1) ++
+  new tacit.WithTraceSinkAlways(0) ++
+  new chipyard.config.WithTraceArbiterMonitor ++
+  new chipyard.WithTacitEncoder ++
+  // new freechips.rocketchip.subsystem.WithoutTLMonitors ++
+  new chipyard.config.WithAsidLen(16) ++
+  new boom.v3.common.WithNMegaBooms(1) ++
+  new chipyard.config.WithSystemBusWidth(128) ++
+  new chipyard.config.AbstractConfig)
+
+class TacitMegaBoomV3RawByteShyDMAConfig extends Config(
+  new tacit.WithTraceSinkRawByte(2) ++
+  new tacit.WithTraceSinkDMA(1, nSource = 4) ++
   new tacit.WithTraceSinkAlways(0) ++
   new chipyard.config.WithTraceArbiterMonitor ++
   new chipyard.WithTacitEncoder ++
@@ -249,5 +273,38 @@ class TacitMediumBoomV4RawByteConfig extends Config(
   new chipyard.config.WithAsidLen(16) ++
   new freechips.rocketchip.subsystem.WithoutTLMonitors ++
   new boom.v4.common.WithNMediumBooms(1) ++
+  new chipyard.config.WithSystemBusWidth(128) ++
+  new chipyard.config.AbstractConfig)
+
+// Stress-test encoder: MegaBoom 4-wide with tiny buffers to trigger buffer pressure quickly
+class WithTacitStressEncoder extends Config((site, here, up) => {
+  case TilesLocated(InSubsystem) => up(TilesLocated(InSubsystem), site) map {
+    case tp: boom.v3.common.BoomTileAttachParams => tp.copy(tileParams = tp.tileParams.copy(
+      traceParams = Some(TraceEncoderParams(
+        encoderBaseAddr = 0x3000000 + tp.tileParams.tileId * 0x1000,
+        buildEncoder = (p: Parameters) => LazyModule(new TacitParallelEncoder(new TraceCoreParams(
+          nGroups = tp.tileParams.core.retireWidth,
+          iretireWidth = 1,
+          xlen = tp.tileParams.core.xLen,
+          iaddrWidth = tp.tileParams.core.xLen
+        ),
+        bufferDepth = 16,
+        coreStages = 5,
+        )(p)),
+        useArbiterMonitor = false
+      )),
+      core = tp.tileParams.core.copy(enableTraceCoreIngress = true)))
+    case other => other
+  }
+})
+
+class TacitMegaBoomV3StressConfig extends Config(
+  new tacit.WithTraceSinkRawByte(2) ++
+  new tacit.WithTraceSinkDMA(1) ++
+  new tacit.WithTraceSinkAlways(0) ++
+  new chipyard.config.WithTraceArbiterMonitor ++
+  new chipyard.WithTacitStressEncoder ++
+  new chipyard.config.WithAsidLen(16) ++
+  new boom.v3.common.WithNMegaBooms(1) ++
   new chipyard.config.WithSystemBusWidth(128) ++
   new chipyard.config.AbstractConfig)
